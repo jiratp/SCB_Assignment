10be69a8cc4c4bd4e35a801e43a36ea0c104a33a
Jirat <blackcats.n@gmail.com>
2019-06-20 11:11:52 +0700

    add config of dependencies hammer.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import * as Hammer from 'hammerjs';
 
 // PAGE MODULE
 import { PagesModule } from './pages/pages.module';
@@ -16,6 +18,23 @@ const appRoutes: Routes = [
   }
 ];
 
+export class MyHammerConfig extends HammerGestureConfig  {
+  overrides = <any>{
+      // override hammerjs default configuration
+      'swipe': { direction: 6, enabled: true   },
+      'pinch': { enabled: true },
+      'rotate': { enabled: true },
+      'touch': { enabled: true }
+  };
+  buildHammer(element: HTMLElement) {
+    const mc = new Hammer(element, {
+      touchAction: 'auto',
+      enable: true,
+      domEvents: true,
+    });
+    return mc;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -28,7 +47,13 @@ const appRoutes: Routes = [
     CommonModule,
     RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
